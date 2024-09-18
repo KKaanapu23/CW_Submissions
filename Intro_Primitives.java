e6ddadf44fd3c1b0f901df3759a796a717b52c0c
KKaanapu23 <hawaiianboyk12@gmail.com>
2024-09-18 11:09:16 -1000

    Create Intro_Primitives.java

---
+++ Intro_Primitives.java
@@ -0,0 +1,31 @@
+
+public class Intro_Primitives {
+
+	public static void main(String[] args) {
+		//Task 1: Print your name
+
+		System.out.println("K     K           A          M                   M");
+		System.out.println("K    K           A A         MM                 MM");
+		System.out.println("K   K           A   A        M M               M M");
+		System.out.println("K  K           A     A       M  M             M  M");
+		System.out.println("K K           A       A      M   M           M   M");
+		System.out.println("KK           A         A     M    M         M    M");
+		System.out.println("K K         AAAAAAAAAAAAA    M     M       M     M");
+		System.out.println("K  K       A             A   M      M     M      M");
+		System.out.println("K   K     A               A  M       M   M       M");
+		System.out.println("K    K   A                 A M        M M        M");
+		System.out.println("K     K A                   AM         M         M");
+	
+		//Task 2: Write code to compute the following math problem
+		
+		System.out.println( ((6.0 * 3.5) - (1.5 * 5.0)) / (55.6 - 30.2) );
+		
+		//Task 3: Write adaptable code that will print out the circumfrence and area of a circle that has a radius of 4.0
+		final double pi = 3.1519;
+		double r = 4.0;
+		
+		System.out.println(2 * pi * r);
+		
+	}
+
+}
